de9ab2eefbb07b5d49e5d3265baae3298ca55409
revanthpobala <revanth.linux@gmail.com>
2017-06-04 18:37:55 -0700

    Added a new controller to display the error message.

---
+++ thousandeyes/src/main/java/com/revanth/twitter/thousandeyes/controller/ErrorHandlerController.java
@@ -0,0 +1,59 @@
+package com.revanth.twitter.thousandeyes.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * Mapping for Error handler.
+ * Created by Revanth on 6/4/2017.
+ */
+@RestController
+@RequestMapping("/error")
+public class ErrorHandlerController implements ErrorController {
+    private final ErrorAttributes errorAttributes;
+
+    @Autowired
+    public ErrorHandlerController(ErrorAttributes errorAttributes) {
+        Assert.notNull(errorAttributes, "Error Attributes must not be null");
+        this.errorAttributes = errorAttributes;
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping
+    public Map<String, Object> error(HttpServletRequest aRequest) {
+        Map<String, Object> body = getErrorAttributes(aRequest, getTraceParameter(aRequest));
+        String trace = (String) body.get("trace");
+        if (trace != null) {
+            String[] lines = trace.split("\n\t");
+            body.put("trace", lines);
+        }
+        return body;
+    }
+
+    private boolean getTraceParameter(HttpServletRequest request) {
+        String parameter = request.getParameter("trace");
+        if (parameter == null) {
+            return false;
+        }
+        return !"false".equals(parameter.toLowerCase());
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest aRequest, boolean includeStackTrace) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(aRequest);
+        return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+    }
+
+}
