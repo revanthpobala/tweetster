5c2f0243ca99de251f35f12ed77e95c19868e60b
revanthpobala <revanth.linux@gmail.com>
2017-06-04 18:35:10 -0700

    Added a new controller to display the error message.

--- thousandeyes/src/main/java/com/revanth/twitter/thousandeyes/ThousandEyesApplication.java
+++ thousandeyes/src/main/java/com/revanth/twitter/thousandeyes/ThousandEyesApplication.java
@@ -15,6 +15,7 @@ public class ThousandEyesApplication {
      *
      * @param args
      */
+
     public static void main(String[] args) {
         SpringApplication.run(ThousandEyesApplication.class, args);
     }
